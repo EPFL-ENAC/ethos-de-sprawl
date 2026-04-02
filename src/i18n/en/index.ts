f4a87f9f47813b639f2743bcd192d0cd6b8292d2
ymarcon <yannick.marcon@epfl.ch>
2026-04-02 19:53:55 +0200

    chore: prepared webapp (wip)

---
+++ src/i18n/en/index.ts
@@ -0,0 +1,23 @@
+// This is just an example,
+// so you can safely delete all default props below
+
+export default {
+  app_title: 'App Title',
+  error_not_found: 'Oops. Nothing here...',
+  filters: 'Filters',
+  it4r_contrib: 'UI/UX design, application development and deployment',
+  layer: {
+    earthquakes: 'Earthquakes',
+  },
+  layers: 'Layers',
+  legends: 'Legends',
+  home: 'Home',
+  introduction: 'Introduction',
+  magnitudes: 'Magnitudes',
+  magnitudes_help: 'Select the range of magnitudes.',
+  number_of_earthquakes: 'Number of earthquakes',
+  reset_filters: 'Reset filters',
+  resources: 'Resources',
+  source_code: 'Source Code',
+  with_tsunami: 'With tsunami'
+};
